f8596ee92e575f09b72905660236b18ac47c7f75
Larry Jones <mrwizard82d1@gmail.com>
2025-11-20 14:20:24 -0600

    Put and take values using a channel

--- core-async/src/core.clj
+++ core-async/src/core.clj
@@ -1,5 +1,7 @@
 (ns core
-  (:require [clojure.core.async :as a :refer [chan >!!]]))
+  (:require [clojure.core.async :as a :refer [chan >!! <!!]]))
+
+;; Be sure to evaluate all these forms in the `core` namespace
 
 ;; Create a channel
 (chan)
@@ -11,10 +13,18 @@
 
 ;; Push values onto a channel from one thread and pull values off
 ;; on another.
-(let [c chan]
-  (future ;; starts a thread executing the next form
+(let [c (chan)]
+  ;; starts a thread executing the next form (a "blocking" `put`
+  ;; operation)
+  (future
     (doseq [x (range 1 5)]
       ;; Put (with blocking) the value, `x` onto our channel, `c`
       ;; "The first `!` says, 'This is a side-effect.' The second
       ;; `!` says that this is blocking."
-      (>!! c x))))
+      (>!! c x)))
+  ;; starts a thread executing the next for (a "blocking" `take`
+  ;; operation)
+  (future
+    (doseq [x (range 1 5)]
+      ;; Take (with blocking) the next value from our channel, `c`
+      (println "from chan" (<!! c)))))
